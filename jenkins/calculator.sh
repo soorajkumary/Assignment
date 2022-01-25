87d4510ab4dff66cb72ca97b660d10fb14aae558
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-25 17:29:27 +0000

    calculator

---
+++ jenkins/calculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Number1=$1
+add_or_sub=$2
+Number2=$3
+if [ $add_or_sub == "add" ]
+then
+        e=`expr $Number1 + $Number2`
+        echo "result: "$e
+elif [ $add_or_sub == "sub" ]
+then
+        f=`expr $Number1 - $Number2`
+        echo "result: "$f
+
+fi
