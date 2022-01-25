e952b54e7cd9cf9fb5e9c92fd35e3a138a18447e
Soorajnaik <98380496+Soorajnaik@users.noreply.github.com>
2022-01-25 21:58:56 +0530

    Update simpleint.sh

--- jenkins/simpleint.sh
+++ jenkins/simpleint.sh
@@ -4,5 +4,5 @@
 Principal_Amount=$1
 Interest=$2
 Time=$3
-r= expr $Principal_Amount \* $Interest \* $Time / 100
-echo $r
+r=`expr $Principal_Amount \* $Interest \* $Time / 100`
+echo "Simple Interest is $r"
