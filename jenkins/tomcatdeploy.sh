0f84f57ca97b9b3138ac3f08b229543d1bb591f4
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-26 11:55:14 +0000

    tom

--- jenkins/tomcatdeploy.sh
+++ jenkins/tomcatdeploy.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-cd /root/webapp/webapp
+cd /home/ubuntu/webapp
 
 mvn clean install
 
-sudo scp /root/webapp/webapp/target/webapp.war  ubuntu@52.12.122.206:/home/ubuntu/apache-tomcat-8.5.75/webapps
+scp /home/ubuntu/webapp/target/webapp.war  ubuntu@54.185.185.197:/home/ubuntu/apache-tomcat-8.5.75/webapps
 
-ssh ubuntu@54.185.185.197<<EOF
-  #sudo cp -R /home/ubuntu/webapp.war /home/ubuntu/apache-tomcat-8.5.75/webapps;
+ssh ubuntu@54.185.185.197 << EOF
   cd /home/ubuntu/apache-tomcat-8.5.75/bin
   sh catalina.sh stop
   sh catalina.sh start
 EOF
 
-#sudo cp -R /home/ubuntu/webapp.war /opt/apache-tomcat-8.5.75/webapps
-
-#sudo -i
-
-#cd /opt/apache-tomcat-8.5.75/bin
-
-#bash catalina.sh stop
-
-#bash catalina.sh start 
