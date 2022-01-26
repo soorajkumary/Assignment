2901e30f6b198baf7f8ba1e0a692a78b1e57e681
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-26 06:02:11 +0000

    salary

---
+++ jenkins/salary.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+basic=$1
+
+dp=`expr $basic \* 50 / 100`
+
+echo "DP amount is: $dp"
+
+a=`expr $basic + $dp`
+
+da=`expr $a \* 35 / 100`
+
+hra=`expr $a \* 8 / 100`
+
+ma=`expr $a \* 3 / 100`
+
+pf=`expr $a \* 10 / 100`
+
+Salary=`expr $basic + $dp + $da + $hra + $ma - $pf`
+
+echo "Salary is: $Salary"
+
+
+
+
