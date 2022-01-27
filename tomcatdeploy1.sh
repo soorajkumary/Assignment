3f47a177fc91125111b2f89ccfc717e5c95cd55b
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-27 09:35:10 +0000

    tom1

--- tomcatdeploy1.sh
+++ tomcatdeploy1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mvn -f /home/ubuntu/webapp/pom.xml clean install
-
-scp /home/ubuntu/webapp/target/webapp.war  ubuntu@34.211.76.234:/home/ubuntu/apache-tomcat-8.5.75/webapps
+scp /webapp/target/webapp.war  ubuntu@34.211.76.234:/home/ubuntu/apache-tomcat-8.5.75/webapps
 
 ssh -o ubuntu@34.211.76.234 << EOF
   cd /home/ubuntu/apache-tomcat-8.5.75/bin
