e754ed770a181cd4007707d10c7930ea5e0b96cb
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-27 09:33:24 +0000

    tom

---
+++ tomcatdeploy1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mvn -f /home/ubuntu/webapp/pom.xml clean install
+
+scp /home/ubuntu/webapp/target/webapp.war  ubuntu@34.211.76.234:/home/ubuntu/apache-tomcat-8.5.75/webapps
+
+ssh -o ubuntu@34.211.76.234 << EOF
+  cd /home/ubuntu/apache-tomcat-8.5.75/bin
+  sh catalina.sh stop
+  sh catalina.sh start
+EOF
+
