7a3b83b4573f5a6f0f2d31a48403f900bc6a36a5
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-31 07:07:41 +0000

    s31

--- jenkins/s3.sh
+++ jenkins/s3.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-aws s3api create-bucket \
-    --bucket sooraj-bucket \
-    --region us-west-2
+aws s3api create-bucket --bucket Soorajbucket --create-bucket-configuration LocationConstraint=eu-west-2
