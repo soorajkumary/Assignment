44ea2c6763a2e7eab4e8b4e9e2da610e30d86b94
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-31 06:15:16 +0000

    1

--- jenkins/ec2.sh
+++ jenkins/ec2.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-0e31b343ec333d397 --count 1 --instance-type t2.micro --key-name Sooraj  --security-group-ids sg-0ba3992b98d2e1fdd --region us-west-2
+aws ec2 run-instances --image-id ami-074251216af698218 --count 1 --instance-type t2.micro --key-name Sooraj  --security-group-ids sg-0ba3992b98d2e1fdd --region us-west-2
