2a8de6d5a9c668494ea8a77aa4f0f693376f7b25
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-31 10:41:15 +0000

    util

---
+++ jenkins/utilization1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+memory=`free -m | awk 'NR==2{print $3/$2*100}'`
+
+disk=`df -m | awk 'NR==2{print $3/$2*100}'`
+
+cpu=`mpstat | awk 'NR==4{print $12/100}'`
+
+c=`echo "Cpu utilization: $cpu%"`
+d=`echo "Memory utilization: $memory%"`
+e=`echo "Disk utilization: $disk"`
+
+message=`echo "$c $d $e"`
+
+[ ! -z "$message" ] && curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${message}\"}" https://hooks.slack.com/services/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
+
+
