4d089888f55332e2b9d6e59797a72b3b7d39e93a
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-01-31 07:01:36 +0000

    s3

--- jenkins/ec2.sh
+++ jenkins/ec2.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 aws ec2 run-instances --image-id ami-074251216af698218 --count 1 --instance-type t2.micro --key-name Sooraj  --security-group-ids sg-0ba3992b98d2e1fdd --region us-west-2
---
+++ jenkins/s3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws s3api create-bucket \
+    --bucket sooraj-bucket \
+    --region us-west-2
