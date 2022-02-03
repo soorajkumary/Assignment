cc02ad3394681c369ab11d6f79a77e2f91e33792
root <root@ip-172-31-45-191.us-west-2.compute.internal>
2022-02-03 03:58:58 +0000

    do2

--- docker/tomcat.sh
+++ docker/tomcat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /webapp
 mvn clean install
-cp /webapp/target/webapp.war /opt/tomcat/apache-tomcat-10.0.16/webapps
-cd /opt/tomcat/apache-tomcat-10.0.16/bin
+cp /webapp/target/webapp.war /Assignment/docker/apache-tomcat-10.0.16/webapps
+cd /Assignment/docker/apache-tomcat-10.0.16/bin
 bash catalina.sh stop
 bash catalina.sh start
 sleep 1000
